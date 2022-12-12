f4ff27f67bc962f9f21e1726e840bf840ceaee86
Renan Bitencourt Pacheco <renan.pacheco@pampa.compasso>
2022-12-12 15:41:51 -0300

    novas informações do dashboard do cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 't1y2un',
+  projectId: "cgsy24",
   e2e: {
     viewportHeight: 768,
     viewportWidth: 1366,
